8f0ff4b1b22b7972da33a0f4ef726e237f2d6c82
Merl <merl.chandana@gmail.com>
2020-07-09 00:41:57 +0530

    add 2 comments

--- Salary_ML.py
+++ Salary_ML.py
@@ -1,3 +1,4 @@
+# This is a comment I made
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -5,6 +6,8 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_squared_error
 import seaborn as sns
+
+# Here's another comment
 
 def clean_data(df):
     '''
